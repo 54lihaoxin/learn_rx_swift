60bac461a98953e92ea8c2b831af5f7c51bc74f0
Haoxin Li <54lihaoxin@gmail.com>
2018-11-12 14:03:02 -0800

    Initial commit with the RxSwift pods

---
+++ LearnRxSwift/LearnRxSwift/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  LearnRxSwift
+//
+//  Created by Li, Haoxin on 11/12/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
